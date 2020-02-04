bbed9644f908f9f8cccb7e1e74ff12b5689a451d
siraegi <shihabbong11@gmail.com>
2020-02-04 13:00:08 +0900

    connection server and client

--- Server/index.js
+++ Server/index.js
@@ -9,3 +9,9 @@ setInterval(() => {
     server.onUpdate();
 }, 100, 0);
 */
+io.on('connection', function(socket){
+    console.log('Connection Made!');
+    socket.on('disconnect', function(){
+        console.log('A player has disconnected');
+    });
+});
