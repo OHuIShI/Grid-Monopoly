a78aaa1e122ede0f423367c5d284982559f15cc2
siraegi <shihabbong11@gmail.com>
2020-02-04 13:26:00 +0900

    initialize player code

--- Server/index.js
+++ Server/index.js
@@ -1,7 +1,11 @@
 let io = require('socket.io')(process.env.PORT || 52300);
 //let Server = require('./Classes/Server')
+let Player = require('./Classes/Player.js');
 
 console.log('Server has started');
+
+let players = [];
+let sockets = [];
 /*
 let server = new Server();
 
@@ -11,7 +15,34 @@ setInterval(() => {
 */
 io.on('connection', function(socket){
     console.log('Connection Made!');
+
+    let player = new Player();
+    let thisPlayerID = player.id;
+    players[thisPlayerID] = player.id;
+    sockets[thisPlayerID] = socket;
+
+    //Tell the client that this is our id for the server
+    socket.emit('register', {id: thisPlayerID});
+    socket.emit('spawn', player); //Tell myself I have spawned
+    socket.broadcast.emit('spawn', player); //Tell other I have spawned
+
+    //Tell myself about everyone else in the game
+    for(var playerID in players) {
+        if(playerID != thisPlayerID) {
+            socket.emit('spawn', players[playerID]);
+        }
+    }
+
+    socket.on('updatePosition', function(data) {
+        player.position.x = data.position.x;
+        player.position.y = data.position.y;
+
+        socket.broadcast.to(connection.lobby.id).emit('updatePosition', player);
+    });
+
     socket.on('disconnect', function(){
         console.log('A player has disconnected');
+        delete players[thisPlayerID];
+        delete sockets[thisPlayerID];
     });
 });
