eec2b12f4142e61c062b0ebc7d82d1be421f26ec
Kim Juhui <kjh107704@gmail.com>
2020-02-04 12:14:24 +0900

    initialize server

---
+++ Server/index.js
@@ -0,0 +1,11 @@
+let io = require('socket.io')(process.env.PORT || 52300);
+//let Server = require('./Classes/Server')
+
+console.log('Server has started');
+/*
+let server = new Server();
+
+setInterval(() => {
+    server.onUpdate();
+}, 100, 0);
+*/
