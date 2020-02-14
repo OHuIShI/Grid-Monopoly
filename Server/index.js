c5f7ff1d0cbff99b647ad5a26e622be241ba6e94
Kim Juhui <kjh107704@gmail.com>
2020-02-14 14:45:11 +0900

    update updateLandData

--- Server/index.js
+++ Server/index.js
@@ -232,6 +232,7 @@ io.on('connection', function(socket){
                 landManager.landData[landIndex].status.building = true;
                 break;
             case "Contract":
+                landManager.landData[landIndex].status.contract = true;
                 break;
             case "Acquire":
                 break;
